b7d143219f56e72f5a86bf06114edf06a55891cc
Sand583 <evandstrom@gmail.com>
2024-11-18 21:08:50 -0500

    Added limited CVE check

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from datetime import datetime
 TIMEOUT = 5
 DEFAULT_PORTS = [21, 22, 23, 25, 53, 80, 443, 8080, 8443, 137, 139, 445, 1433, 1434, 3306, 3389]
 REVC_SIZE = 1024
+MAX_CVES = 5
 
 MAX_THREADS = 16
 open_ports = 0 
 open_lock = threading.Lock()
 print_lock = threading.Lock()
-
 
 def scan_port_wrapper(port):
     if scan_port(port):
@@ -30,27 +30,42 @@ def get_service(s, port):
     except OSError:
         return "Unknown service"
 
-def grab_banner(s, port):
+#TODO add support for more services
+def grab_banner(s, port, service):
     try:
-        protocol = get_service(s, port)
-
-        if protocol == 'http' or protocol == 'https':
+        if service == 'http' or service == 'https':
             s.send(b'GET /\n')
-            return s.recv(REVC_SIZE).decode().strip().split('\n')[2]
-        
-        #TODO add cases for other protocols
-        
+            result = s.recv(REVC_SIZE).decode().strip().split('\n')[2]
+            exp = re.search(r"(\S+)/([\d\.]+)", result)
+            return f"{exp.group(1)} {exp.group(2)}"        
         else:
             return s.recv(REVC_SIZE).decode().strip()
     
     except Exception as e:
         return f"ERROR grabbing banner, \"{e}\""
 
-def check_cves(s, port):
-    #TODO: should check the port for any common vulnerabilites and exposures (CVE)
-    #I know there's several libraries, external databases, APIs out there (PyCVESearch, NVD, CVE, Shodan, Vulners, nmap, etc)
-    #But I haven't gotten them to work
-    pass 
+def check_cves(keywords):
+    try:
+        url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+
+        params = {
+            "keywordSearch": keywords,
+            "resultsPerPage": MAX_CVES
+        }
+
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            result = response.json()
+            cves = ""
+            for vuln in result["vulnerabilities"]:
+                cve_id = vuln["cve"]["id"]
+                description = vuln["cve"]["descriptions"][0]["value"] #assumes first description is in English
+                cves += f"\n\t{cve_id}: {description}"
+            return cves
+        else:
+            return "Found nothing"
+    except Exception as e:
+        return "Error getting CVEs"
 
 def scan_port(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -60,20 +75,25 @@ def scan_port(port):
         result = s.connect_ex((target_ip, port))
 
         if result != 0:
-            #with print_lock:
-            #    print(f"Port {port} is closed.")
+            if args.v:
+                with print_lock:
+                    print(f"Port {port} is closed.")
             return False
 
         message = f"Port {port} is open."
+        
+        if args.s or args.b or args.v:
+            service = get_service(s, port)
+            if args.s:
+                message += f"\n\tService: {service}"
 
-        if args.s:
-            message += f"\n\tService: {get_service(s, port)}."
+            if args.b or args.c:
+                banner = grab_banner(s, port, service)
+                if args.b:
+                    message += f"\n\tBanner: {banner}"
 
-        if args.b:
-            message += f"\n\tBanner: {grab_banner(s, port)}."
-
-        if args.v:
-            message += f"\n\tCVE(s): {check_cves(s, port)}."
+                if args.c:
+                    message += f"\n\tCVE(s): {check_cves(banner)}."
 
         with print_lock:
             print(message)
@@ -82,10 +102,11 @@ def scan_port(port):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-a', action='store_true', help='flag for all')
-    parser.add_argument('-s', action='store_true', help='flag for getting the service')
-    parser.add_argument('-b', action='store_true', help='flag for banner grabbing')
-    parser.add_argument('-v', action='store_true', help='flag to check for CVEs')
+    parser.add_argument('-a', action='store_true', help='Flag for all') #does all of the below 
+    parser.add_argument('-s', action='store_true', help='Get service running on port') #get's what service is running on the port
+    parser.add_argument('-b', action='store_true', help='Banner grabbing') #get software banner information from the port 
+    parser.add_argument('-c', action='store_true', help='Check for CVEs (Warning: will considerably slow down scan)') #checks Common Vulnerabilites and Exposures (CVE) 
+    parser.add_argument('-v', action='store_true', help='Verbose mode (prints closed ports)') #print if a port is closed 
 
     global args 
     args = parser.parse_args()
@@ -93,6 +114,7 @@ if __name__ == "__main__":
     if args.a:
         args.s = True
         args.b = True
+        args.c = True
         args.v = True
 
     print("--- Port Scanner ---")
